ecb7227aa7368d860dc9565fa7cea8cf0e43dd34
David Wallace Croft <david@CroftSoft.com>
2024-04-01 07:02:10 -0500

    version 0.1.0

---
+++ README.md
@@ -0,0 +1,10 @@
+# com-croftsoft-lib
+
+[![MIT licensed][mit-badge]][mit-url]
+
+[mit-badge]: https://img.shields.io/badge/license-MIT-blue.svg
+[mit-url]: https://github.com/david-wallace-croft/com-croftsoft-lib/blob/main/LICENSE.txt
+
+- Re-export of com-croftsoft-lib-* crates
+- https://www.CroftSoft.com/
+- First published to crates.io on 2024-04-01
---
+++ src/lib.rs
@@ -0,0 +1,15 @@
+// =============================================================================
+//! - Re-export of com-croftsoft-lib-* crates
+//! - <https://www.CroftSoft.com/>
+//! - Updated: 2024-04-01
+//! - Created: 2024-04-01
+// =============================================================================
+
+pub mod com_croftsoft_lib {
+  #[doc(inline)]
+  pub use com_croftsoft_lib_animation::*;
+  #[doc(inline)]
+  pub use com_croftsoft_lib_role::*;
+  #[doc(inline)]
+  pub use com_croftsoft_lib_string::*;
+}
