f143a23d905747c79534c9616876d3a16f07c5a3
David Wallace Croft <david@CroftSoft.com>
2024-04-02 07:21:49 -0500

    separate modules

--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,21 @@
 // =============================================================================
 //! - Re-export of com-croftsoft-lib-* crates
 //! - <https://www.CroftSoft.com/>
-//! - Updated: 2024-04-01
+//! - Updated: 2024-04-02
 //! - Created: 2024-04-01
 // =============================================================================
 
-pub mod com_croftsoft_lib {
+pub mod animation {
   #[doc(inline)]
   pub use com_croftsoft_lib_animation::*;
+}
+
+pub mod role {
   #[doc(inline)]
   pub use com_croftsoft_lib_role::*;
+}
+
+pub mod string {
   #[doc(inline)]
   pub use com_croftsoft_lib_string::*;
 }
